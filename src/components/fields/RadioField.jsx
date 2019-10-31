e587c699794e707cdaf928292a2135a6f1d10119
Aye Myat Thin Aung <ayemyatthinaung@achromex.com>
2019-10-31 10:34:48 +0630

    radio field added

---
+++ src/components/fields/RadioField.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'redux-form';
+import { FormGroup, Radio, RadioGroup } from 'rsuite';
+
+const renderField = fields => {
+  const {
+    name,
+    label,
+    className,
+    style,
+    id,
+    input: { value, onChange, ...rest },
+    // meta: { touched, error },
+    disabled,
+    isRequired,
+    checked,
+    defaultChecked,
+    inline,
+  } = fields;
+  return (
+    <FormGroup className="pt-3 m-0" style={style}>
+      <label className={className}>{label}</label>
+      <RadioGroup>
+        <Radio
+          name={name}
+          label={label}
+          // options={[
+          //   { label: 'Male', value: 'MALE' },
+          //   { label: 'Female', value: 'FEMALE' },
+          //   { label: 'Other', value: '' },
+          // ]}
+          className={className}
+          value="A"
+          onChange={onChange}
+          {...rest}
+          id={id}
+          disabled={disabled}
+          isRequired={isRequired}
+          defaultChecked={defaultChecked}
+          checked={checked}
+          inline={inline}
+        />
+      </RadioGroup>
+    </FormGroup>
+  );
+};
+
+const RadioField = ({
+  label,
+  name,
+  id,
+  className,
+  language,
+  style,
+  disabled,
+  isRequired,
+  defaultChecked,
+  checked,
+  inline,
+}) => (
+  <Field
+    component={renderField}
+    name={name}
+    id={id}
+    label={label}
+    className={className}
+    style={style}
+    language={language}
+    disabled={disabled}
+    isRequired={isRequired}
+    defaultChecked={defaultChecked}
+    checked={checked}
+    inline={inline}
+  />
+);
+
+RadioField.propTypes = {
+  label: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  disabled: PropTypes.bool,
+  isRequired: PropTypes.bool,
+  checked: PropTypes.bool,
+  inline: PropTypes.bool,
+};
+
+RadioField.defaultProps = {
+  label: 'Gender',
+  name: 'gender',
+  id: 'gender',
+  isRequired: false,
+  disabled: false,
+  checked: false,
+  inline: false,
+};
+
+export default RadioField;
